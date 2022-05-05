b0bbb2f39c9511e83d172b575d98d44b121402f3
Jasmin <94010495+JaskoM95@users.noreply.github.com>
2022-05-05 17:56:39 +0200

    JaskoM95 / vrsta_filma

---
+++ vjezbanje2.js
@@ -0,0 +1,77 @@
+var app = angular.module ('moj_modul',[]) ;
+
+app.controller ('glavni', function ($scope) {
+
+
+
+    $scope.filmovi = [
+
+        {
+            ime: "Me Before You	",
+            vrsta:"drama"
+        },
+        {
+            ime: "The Magnificent Seven		",
+            vrsta:"western"
+        },
+
+        {
+            ime: "The Revenant	",
+            vrsta:"western"
+        },
+
+        {
+            ime: "Django Unchained",
+            vrsta:"western"
+        },
+
+        {
+            ime: "Lights Out",
+            vrsta:"horor"
+        },
+
+        {
+            ime: "The Shallow",
+            vrsta:"horor"
+        },
+
+        {
+            ime: "The Conjuring",
+            vrsta:"horor"
+        },
+
+        {
+            ime: "The Huntsman:Winter's war		",
+            vrsta:"drama"
+        },
+
+        {
+            ime: "Bad Moms		",
+            vrsta:"comedy"
+        },
+
+        {
+            ime: "Ice Age:Collision Course		",
+            vrsta:"comedy"
+        },
+        {
+            ime: "Ghostbusters		",
+            vrsta:"comedy"
+        },
+
+
+
+
+
+
+    ]
+
+
+
+
+
+
+
+
+
+})
